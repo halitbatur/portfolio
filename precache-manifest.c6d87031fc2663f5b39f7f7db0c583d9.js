016a1f150b86956e957cb4d3e1d1be8da3c8eb72
Halit Batur <halitfuatbatur@gmail.com>
2020-06-28 21:51:48 +0300

    Updates

---
+++ precache-manifest.c6d87031fc2663f5b39f7f7db0c583d9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81898aca1b2f3f43ebb026ca475aa552",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "10097b25f47ce0853cfe",
+    "url": "/portfolio/static/css/main.303565f5.chunk.css"
+  },
+  {
+    "revision": "1fca1d432d74091030b1",
+    "url": "/portfolio/static/js/2.d3e87315.chunk.js"
+  },
+  {
+    "revision": "a986d777e7ab357b3f79ca1eede38fb5",
+    "url": "/portfolio/static/js/2.d3e87315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10097b25f47ce0853cfe",
+    "url": "/portfolio/static/js/main.5fd16a1f.chunk.js"
+  },
+  {
+    "revision": "b1dac7aa291e9cb10de8",
+    "url": "/portfolio/static/js/runtime-main.38b1344e.js"
+  },
+  {
+    "revision": "a002b22af861c17602a5f67dad326832",
+    "url": "/portfolio/static/media/about.a002b22a.svg"
+  },
+  {
+    "revision": "16a5368ee99ee84f715253069c634a28",
+    "url": "/portfolio/static/media/contact.16a5368e.svg"
+  },
+  {
+    "revision": "4b8fd935e0a7a684bf10d2e194d3b1cf",
+    "url": "/portfolio/static/media/github.4b8fd935.svg"
+  },
+  {
+    "revision": "0b69acce4a1bcdbac9207d7b52792567",
+    "url": "/portfolio/static/media/linkedin.0b69acce.svg"
+  },
+  {
+    "revision": "0223118b10e63afe5bf3ea79a9a644b1",
+    "url": "/portfolio/static/media/movie-project.0223118b.png"
+  },
+  {
+    "revision": "370ede085e692a1877556e4fc3fda01e",
+    "url": "/portfolio/static/media/portfolio.370ede08.png"
+  },
+  {
+    "revision": "2d9fdfb701f19cd7b8a1ecebb127646d",
+    "url": "/portfolio/static/media/programmer4.2d9fdfb7.svg"
+  },
+  {
+    "revision": "e1de1489c485491148df78d3c054fee5",
+    "url": "/portfolio/static/media/to-doer.e1de1489.png"
+  }
+]);
